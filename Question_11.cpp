44360d5b46fd9eca606927cfcd460c72081104d7
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 19:33:28 +0530

    Create Question_11.cpp

---
+++ Question_11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float p,r,t;
+cout << "Enter Principal Amount, Rate of Interest & Time Period respectively : ";
+cin >> p >> r >> t;
+cout << "Simple Interest = " << (p*r*t)/100; 
+}
