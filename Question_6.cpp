fb8c9fff47cce3de27aab8e0a78fc08e27c67b0f
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 18:40:04 +0530

    Create Question_6.cpp

---
+++ Question_6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+float a;
+cout << "Enter number : ";
+cin >> a;
+cout << a << "^(1/2)" << "=" << pow(a,(0.5));
+return 0;
+}
