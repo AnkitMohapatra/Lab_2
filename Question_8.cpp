ab700ce664f83951693a0548c1e3d54a64028eff
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 18:47:23 +0530

    Create Question_8.cpp

---
+++ Question_8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+float a,b;
+cout << "Enter base & height of triangle respectively : ";
+cin >> a >> b;
+cout << "Area : " << 0.5*a*b;
+return 0;
+}
