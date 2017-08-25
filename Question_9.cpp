5a3aa5f5af4390941633d3947f15c8b79284cbbd
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 18:50:10 +0530

    Create Question_9.cpp

---
+++ Question_9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+float a;
+cout << "Enter side of the equilateral triangle : ";
+cin >> a;
+cout << "Area : " << (pow(3,0.5)/4)*a*a;
+return 0;
+}
