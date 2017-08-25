76c7dce5e5079ecf72c224a32dc296c0bbe7c88c
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 18:14:42 +0530

    Create Question_1.cpp

---
+++ Question_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float a;
+cout << "Enter value in centimeters : ";
+cin >> a;
+cout << "Value in meters : " << a / 100 << "\n" << "Value in kilometers : " << a/100000;
+return 0;
+}
