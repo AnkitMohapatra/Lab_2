7f647a686f057c8aeb3b42c3f91e8d1a4ffbba45
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 18:17:11 +0530

    Create Question_2.cpp

---
+++ Question_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float a;
+cout << "Enter value in celsius : ";
+cin >> a;
+cout << "Value in farhenheit : " << ((a / 5) * 9) + 32;
+return 0;
+}
