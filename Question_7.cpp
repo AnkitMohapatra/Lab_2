98573f4d42d76abdc5e61b8f795e5fdaa6427e3a
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-25 18:45:12 +0530

    Create Question_7.cpp

---
+++ Question_7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+float a,b;
+cout << "Enter both the angles respectively(in degrees) : ";
+cin >> a >> b;
+cout << "Third angle = " << 180-(a+b);
+return 0;
+}
