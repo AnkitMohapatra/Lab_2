b2e935d9223524e68d6630f71ebf2e609cdc4473
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-08-29 15:33:20 +0530

    Update Question_12.cpp

--- Question_12.cpp
+++ Question_12.cpp
@@ -6,5 +6,5 @@ int main() {
 float p,r,t,n;
 cout << "Enter Principal Amount, Rate of Interest,Time Period & Number of Times Interest is Compounded per Year respectively : ";
 cin >> p >> r >> t >> n;
-cout << "Compound Interest = " << pow((p*(r/n)),(n*t)); 
+cout << "Compound Interest = " << pow((p*((r/100)/n)),(n*t)); 
 }
